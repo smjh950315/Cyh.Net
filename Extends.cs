9f3d9a9eb989592c8682b5cf2e284a8653c381aa
smjh950315 <103402409+smjh950315@users.noreply.github.com>
2024-04-01 20:48:14 +0800

    init

---
+++ Extends.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Cyh.Net
+{
+    public static class Extends
+    {
+        public static bool IsNullOrEmpty([NotNullWhen(false)] this IEnumerable? values) {
+            return values == null || !values.GetEnumerator().MoveNext();
+        }
+    }
+}
