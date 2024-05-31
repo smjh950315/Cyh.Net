94564a38cdadfb0d9baa2b3b32cf83b47b8b333f
smjh950315 <103402409+smjh950315@users.noreply.github.com>
2024-05-31 17:52:41 +0800

    .

---
+++ Native/LifeTimeHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Xml.Linq;
+
+namespace Cyh.Net.Native {
+    /// <summary>
+    /// 處理非託管資源生命週期的類別
+    /// </summary>
+    public class LifeTimeHandler : IDisposable {
+        private bool m_is_disposed;
+        /// <summary>
+        /// 釋放函數
+        /// </summary>
+        private unsafe delegate*<void*, void> m_release_callback;
+        /// <summary>
+        /// 非託管資源指標的記憶體位址
+        /// </summary>
+        private unsafe void** m_data_address;
+
+        protected virtual void Dispose(bool disposing) {
+            if (!this.m_is_disposed) {
+                if (disposing) { }
+
+                unsafe {
+                    if (this.m_release_callback != null && this.m_data_address != null) {
+                        if(*this.m_data_address != null) {
+                            this.m_release_callback(*this.m_data_address);
+                            *this.m_data_address = null;
+                        }
+                        this.m_release_callback = null;
+                        this.m_data_address = null;
+                    }
+                }
+
+                this.m_is_disposed = true;
+            }
+        }
+
+        ~LifeTimeHandler() {
+            this.Dispose(disposing: false);
+        }
+
+        public void Dispose() {
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// 處理非託管資源生命週期的類別
+        /// </summary>
+        /// <param name="address_of_ptr">非託管資源指標的記憶體位址</param>
+        /// <param name="release">釋放函數</param>
+        unsafe public LifeTimeHandler(void** address_of_ptr, delegate*<void*, void> release) {
+            this.m_data_address = address_of_ptr;
+            this.m_release_callback = release;
+        }
+    }
+}
--- Native/Utf8String.cs
+++ Native/Utf8String.cs
@@ -7,55 +7,67 @@ namespace Cyh.Net.Native {
     public unsafe struct Utf8String
         : IEquatable<string>
         , IDisposable {
-        void* _Chars;
-        nuint _Length;
 
-        void _Load_From_Bytes(void* ptr, nuint count) {
-            if (this._Chars != null) { throw new InvalidOperationException(""); }
-            this._Chars = Utilities.Allocate(count);
-            Buffer.MemoryCopy(ptr, this._Chars, count, count);
-            this._Length = count;
+        void* m_chars;
+        nuint m_length;
+        nuint m_disposed = 0;
+        void load_from_bytes(void* ptr, nuint count) {
+            if (this.m_chars != null) { throw new InvalidOperationException(""); }
+            this.m_chars = Utilities.Allocate(count);
+            Buffer.MemoryCopy(ptr, this.m_chars, count, count);
+            this.m_length = count;
         }
 
-        public bool IsDisposed = false;
-        public readonly int Length => (int)this._Length;
-        public readonly int Count => this.Length;
+        /// <summary>
+        /// 字串資料是否被釋放
+        /// </summary>
+        public readonly bool IsDisposed => this.m_disposed != 0;
+
+        /// <summary>
+        /// 字串資料的位元長度
+        /// </summary>
+        public readonly int Length => (int)this.m_length;
 
         public Utf8String(string? @string) {
             if (@string.IsNullOrEmpty()) { return; }
             byte[] u8str = Encoding.UTF8.GetBytes(@string);
             fixed (byte* ptr = u8str) {
-                this._Load_From_Bytes(ptr, (nuint)u8str.Length);
+                this.load_from_bytes(ptr, (nuint)u8str.Length);
             }
         }
+
         public Utf8String(Utf8String? other) {
             if (other == null) { return; }
             if (other.Value.IsDisposed) { return; }
-            if (other.Value.Length != 0 && other.Value._Chars != null) {
-                this._Load_From_Bytes(other.Value._Chars, other.Value._Length);
+            if (other.Value.Length != 0 && other.Value.m_chars != null) {
+                this.load_from_bytes(other.Value.m_chars, other.Value.m_length);
             }
         }
+
         public override readonly string ToString() {
-            if (this._Length == 0) { return String.Empty; }
-            return Encoding.UTF8.GetString((byte*)this._Chars, this.Length);
+            if (this.m_length == 0) { return String.Empty; }
+            return Encoding.UTF8.GetString((byte*)this.m_chars, this.Length);
         }
+
         public bool Equals(string? other) {
             if (other == null) { return false; }
             return this.ToString() == other.ToString();
         }
+
         public bool Equals(Utf8String other) {
             if (other.Length != this.Length) { return false; }
-            return Utilities.IsBytesEqual(this._Chars, other._Chars, this.Length);
+            return Utilities.IsBytesEqual(this.m_chars, other.m_chars, this.Length);
         }
 
         public override int GetHashCode() {
             return base.GetHashCode();
         }
+
         public void Dispose() {
-            Utilities.Free(this._Chars);
-            this._Chars = null;
-            this._Length = 0;
-            this.IsDisposed = true;
+            Utilities.Free(this.m_chars);
+            this.m_chars = null;
+            this.m_length = 0;
+            this.m_disposed = 1;
         }
 
         public static implicit operator Utf8String(string? value) {
