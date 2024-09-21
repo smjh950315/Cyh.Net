24f76689801daa768653b6478df130381c887ffa
smjh950315 <103402409+smjh950315@users.noreply.github.com>
2024-09-22 00:09:32 +0800

    update: linq expression tool

--- Extends.cs
+++ Extends.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 using Cyh.Net.DependencyInjection;
+using Cyh.Net.Internal;
 
-namespace Cyh.Net {
-    public static class Extends {
+namespace Cyh.Net
+{
+    public static class Extends
+    {
         /// <summary>
         /// Determines whether the specified value is null or empty.
         /// </summary>
         /// <returns>Return true if the input sequence is null or empty.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsNullOrEmpty([NotNullWhen(false)] this IEnumerable? values) {
+        public static bool IsNullOrEmpty([NotNullWhen(false)] this IEnumerable? values)
+        {
             return values == null || !values.GetEnumerator().MoveNext();
         }
 
@@ -22,7 +27,8 @@ namespace Cyh.Net {
         /// </summary>
         /// <returns>Return true if the sequence contains current value, otherwise false.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsAnyOf<T>(this T value, IEnumerable<T> values) {
+        public static bool IsAnyOf<T>(this T value, IEnumerable<T> values)
+        {
             return values.Contains(value);
         }
 
@@ -62,11 +68,15 @@ namespace Cyh.Net {
         /// <param name="output">Json string from serialized object if completed without error, otherwise null</param>
         /// <returns>Whether serialized without error</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool TrySerialize<T>(this T? value, [NotNullWhen(true)] out string? output, JsonSerializerOptions? options = null) {
-            try {
+        public static bool TrySerialize<T>(this T? value, [NotNullWhen(true)] out string? output, JsonSerializerOptions? options = null)
+        {
+            try
+            {
                 output = value.Serialize(options);
                 return !output.IsNullOrEmpty();
-            } catch {
+            }
+            catch
+            {
                 output = null;
                 return false;
             }
@@ -78,11 +88,15 @@ namespace Cyh.Net {
         /// <param name="output">Object model from deserialized string if completed without error, otherwise null</param>
         /// <returns>Whether deserialized without error</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool TryDeserialize<T>(this string? str, [NotNullWhen(true)] out T? output, JsonSerializerOptions? options = null) {
-            try {
+        public static bool TryDeserialize<T>(this string? str, [NotNullWhen(true)] out T? output, JsonSerializerOptions? options = null)
+        {
+            try
+            {
                 output = str.Deserialize<T>(options);
                 return output != null;
-            } catch {
+            }
+            catch
+            {
                 output = default;
                 return false;
             }
@@ -109,12 +123,14 @@ namespace Cyh.Net {
         /// </summary>
         /// <returns>True if element is equal in both collection</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsEqualTo<T>(this IEnumerable<T>? values, IEnumerable<T>? others) where T : IEquatable<T> {
+        public static bool IsEqualTo<T>(this IEnumerable<T>? values, IEnumerable<T>? others) where T : IEquatable<T>
+        {
             if (values.IsNullOrEmpty() || others.IsNullOrEmpty()) { return false; }
             if (values.Count() != others.Count()) { return false; }
             var itSelf = values.GetEnumerator();
             var itOther = others.GetEnumerator();
-            while (itSelf.MoveNext() && itOther.MoveNext()) {
+            while (itSelf.MoveNext() && itOther.MoveNext())
+            {
                 if (!itSelf.Current.Equals(itOther.Current)) { return false; }
             }
             return true;
@@ -124,7 +140,8 @@ namespace Cyh.Net {
         /// Whether the input type is number
         /// </summary>
         /// <returns>True if the input type is number, otherwise false</returns>
-        public static bool IsNumeric(this Type type) {
+        public static bool IsNumeric(this Type type)
+        {
             if (type == null) { return false; }
             return type.IsAnyOf(Typedef.Numeric);
         }
@@ -133,7 +150,8 @@ namespace Cyh.Net {
         /// Whether the input type is struct
         /// </summary>
         /// <returns>True if the input type is struct, otherwise false</returns>
-        public static bool IsStruct(this Type type) {
+        public static bool IsStruct(this Type type)
+        {
             return type.IsValueType || type.IsEnum;
         }
 
@@ -141,16 +159,23 @@ namespace Cyh.Net {
         /// Whether the input type is unmanaged, and excluding any managed members
         /// </summary>
         /// <returns>True if the input type is unmanaged, otherwise false</returns>
-        public static bool IsUnmanaged(this Type type) {
-            if (type.IsPrimitive || type.IsPointer || type.IsEnum) {
+        public static bool IsUnmanaged(this Type type)
+        {
+            if (type.IsPrimitive || type.IsPointer || type.IsEnum)
+            {
                 return true;
-            } else if (type.IsValueType) {
+            }
+            else if (type.IsValueType)
+            {
                 var members = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-                foreach (var member in members) {
+                foreach (var member in members)
+                {
                     if (!IsUnmanaged(member.FieldType)) return false;
                 }
                 return true;
-            } else {
+            }
+            else
+            {
                 return false;
             }
         }
@@ -159,14 +184,21 @@ namespace Cyh.Net {
         /// Whether the input type contain the StructureLayout attribute
         /// </summary>
         /// <returns>True if the type input contain the StructureLayout attribute, otherwise false</returns>
-        public static bool HasStructureLayout(this Type type) {
-            try {
-                if (!IsStruct(type)) {
+        public static bool HasStructureLayout(this Type type)
+        {
+            try
+            {
+                if (!IsStruct(type))
+                {
                     return false;
-                } else {
+                }
+                else
+                {
                     return type.StructLayoutAttribute != null;
                 }
-            } catch {
+            }
+            catch
+            {
                 return false;
             }
         }
@@ -175,7 +207,8 @@ namespace Cyh.Net {
         /// Indicate whether the type is Nullable`T`
         /// </summary>
         /// <returns>True if the type is Nullable`T`, otherwise false</returns>
-        public static bool IsNullableWrapped(this Type type) {
+        public static bool IsNullableWrapped(this Type type)
+        {
             return type.Name.StartsWith("Nullable");
         }
 
@@ -183,12 +216,16 @@ namespace Cyh.Net {
         /// Get type if not null
         /// </summary>
         /// <returns>Type of Nullable`T`.Value or current type if not Nullable`T`</returns>
-        public static Type GetNotNullType(this Type type) {
-            if (type.IsNullableWrapped()) {
+        public static Type GetNotNullType(this Type type)
+        {
+            if (type.IsNullableWrapped())
+            {
 #pragma warning disable CS8602
                 return type.GetProperty("Value").PropertyType;
 #pragma warning restore CS8602
-            } else {
+            }
+            else
+            {
                 return type;
             }
         }
@@ -196,14 +233,20 @@ namespace Cyh.Net {
         /// <summary>
         /// Print StackTrace with title if current exception is not null, otherwise do nothing
         /// </summary>
-        public static void PrintStack(this Exception? exception, Action<string?>? messageHolder = null) {
+        public static void PrintStack(this Exception? exception, Action<string?>? messageHolder = null)
+        {
             if (exception == null) { return; }
             Action<string?> _messageHolder;
-            if (messageHolder != null) {
+            if (messageHolder != null)
+            {
                 _messageHolder = messageHolder;
-            } else if (Lib.gs_globalMessageHandler != null) {
+            }
+            else if (Lib.gs_globalMessageHandler != null)
+            {
                 _messageHolder = Lib.gs_globalMessageHandler;
-            } else {
+            }
+            else
+            {
                 _messageHolder = Console.WriteLine;
             }
             _messageHolder("===============StackTrace===============");
@@ -213,14 +256,20 @@ namespace Cyh.Net {
         /// <summary>
         /// Print Message with title if current exception is not null, otherwise do nothing
         /// </summary>
-        public static void PrintMessage(this Exception? exception, Action<string?>? messageHolder = null) {
+        public static void PrintMessage(this Exception? exception, Action<string?>? messageHolder = null)
+        {
             if (exception == null) { return; }
             Action<string?> _messageHolder;
-            if (messageHolder != null) {
+            if (messageHolder != null)
+            {
                 _messageHolder = messageHolder;
-            } else if (Lib.gs_globalMessageHandler != null) {
+            }
+            else if (Lib.gs_globalMessageHandler != null)
+            {
                 _messageHolder = Lib.gs_globalMessageHandler;
-            } else {
+            }
+            else
+            {
                 _messageHolder = Console.WriteLine;
             }
             _messageHolder("===============Message===============");
@@ -230,14 +279,20 @@ namespace Cyh.Net {
         /// <summary>
         /// Print Source with title if current exception is not null, otherwise do nothing
         /// </summary>
-        public static void PrintSource(this Exception? exception, Action<string?>? messageHolder = null) {
+        public static void PrintSource(this Exception? exception, Action<string?>? messageHolder = null)
+        {
             if (exception == null) { return; }
             Action<string?> _messageHolder;
-            if (messageHolder != null) {
+            if (messageHolder != null)
+            {
                 _messageHolder = messageHolder;
-            } else if (Lib.gs_globalMessageHandler != null) {
+            }
+            else if (Lib.gs_globalMessageHandler != null)
+            {
                 _messageHolder = Lib.gs_globalMessageHandler;
-            } else {
+            }
+            else
+            {
                 _messageHolder = Console.WriteLine;
             }
             _messageHolder("===============Source===============");
@@ -248,7 +303,8 @@ namespace Cyh.Net {
         /// Print all human readable message of an exception if not null
         /// </summary>
         /// <param name="exception"></param>
-        public static void Print(this Exception? exception, Action<string?>? messageHolder = null) {
+        public static void Print(this Exception? exception, Action<string?>? messageHolder = null)
+        {
             if (exception == null) { return; }
             exception.PrintSource(messageHolder);
             exception.PrintMessage(messageHolder);
@@ -260,7 +316,8 @@ namespace Cyh.Net {
         /// </summary>
         /// <param name="parent">Type of parent object</param>
         /// <returns>True if current type is child of <paramref name="parent"/>, otherwise false</returns>
-        public static bool IsChildOf(this Type type, Type parent) {
+        public static bool IsChildOf(this Type type, Type parent)
+        {
             return type.IsSubclassOf(parent);
         }
 
@@ -268,7 +325,8 @@ namespace Cyh.Net {
         /// Indicate whether current type is dereived from <typeparamref name="T"/>
         /// </summary>
         /// <returns>True if current type is child of <typeparamref name="T"/>, otherwise false</returns>
-        public static bool IsChildOf<T>(this Type type) {
+        public static bool IsChildOf<T>(this Type type)
+        {
             return type.IsChildOf(typeof(T));
         }
 
@@ -277,7 +335,8 @@ namespace Cyh.Net {
         /// </summary>
         /// <param name="parent">Type of parent object</param>
         /// <returns>True if current type is child of <paramref name="parent"/>, otherwise false</returns>
-        public static bool IsParentOf(this Type type, Type child) {
+        public static bool IsParentOf(this Type type, Type child)
+        {
             return child.IsSubclassOf(type);
         }
 
@@ -285,7 +344,8 @@ namespace Cyh.Net {
         /// Indicate whether current type dereived from <typeparamref name="T"/>
         /// </summary>
         /// <returns>True if current type is child of <typeparamref name="T"/>, otherwise false</returns>
-        public static bool IsParentOf<T>(this Type type) {
+        public static bool IsParentOf<T>(this Type type)
+        {
             return type.IsParentOf(typeof(T));
         }
 
@@ -295,16 +355,21 @@ namespace Cyh.Net {
         /// <param name="type">Dest type</param>
         /// <param name="constructArgs">constructor parameters</param>
         /// <returns>Instance of <paramref name="type"/> or null if failure or no matched constructor</returns>
-        public static object? Construct(this Type type, params object[] constructArgs) {
-            try {
+        public static object? Construct(this Type type, params object[] constructArgs)
+        {
+            try
+            {
                 Type[] constructorTypes = new Type[constructArgs.Length];
-                for (int i = 0; i < constructorTypes.Length; i++) {
+                for (int i = 0; i < constructorTypes.Length; i++)
+                {
                     constructorTypes[i] = constructArgs[i].GetType();
                 }
                 var constructor = type.GetConstructor(constructorTypes);
                 if (constructor == null) { return null; }
                 return constructor.Invoke(constructArgs);
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 e.Print();
                 return null;
             }
@@ -316,7 +381,8 @@ namespace Cyh.Net {
         /// <param name="type">Dest type</param>
         /// <param name="constructArgs">constructor parameters</param>
         /// <returns>Instance of <paramref name="type"/> or null if failure or no matched constructor</returns>
-        public static bool Construct(this Type type, [NotNullWhen(true)] out object? instance, params object[] constructArgs) {
+        public static bool Construct(this Type type, [NotNullWhen(true)] out object? instance, params object[] constructArgs)
+        {
             instance = type.Construct(constructArgs);
             return instance != null;
         }
@@ -327,7 +393,8 @@ namespace Cyh.Net {
         /// <typeparam name="T">Type of service to get</typeparam>
         /// <param name="serviceInstance">The instance of service</param>
         /// <returns>True if service if found, otherwise false</returns>
-        public static bool GetInjectedService<T>(this IServiceProvider serviceProvider, [NotNullWhen(true)] out T? serviceInstance) where T : class {
+        public static bool GetInjectedService<T>(this IServiceProvider serviceProvider, [NotNullWhen(true)] out T? serviceInstance) where T : class
+        {
             serviceInstance = serviceProvider.GetService(typeof(T)) as T;
             return serviceInstance != null;
         }
@@ -337,9 +404,11 @@ namespace Cyh.Net {
         /// </summary>
         /// <typeparam name="T">Service type</typeparam>
         /// <param name="implFactory">The implement of service factory</param>
-        public static void InjectScopedIfNotExisting<T>(this IDependencyInjectionData diData, Func<IServiceProvider, T> implFactory) where T : class {
+        public static void InjectScopedIfNotExisting<T>(this IDependencyInjectionData diData, Func<IServiceProvider, T> implFactory) where T : class
+        {
             object? service = diData.GetService(typeof(T));
-            if (service == null) {
+            if (service == null)
+            {
                 diData.AddScoped(typeof(T), implFactory);
             }
         }
@@ -349,9 +418,46 @@ namespace Cyh.Net {
         /// </summary>
         /// <typeparam name="T">Service type</typeparam>
         /// <param name="factoryMethod">The implement of service factory</param>
-        public static void InjectScopedIfNotExisting<T>(this IDependencyInjectionData diService, Func<T?> factoryMethod) where T : class {
+        public static void InjectScopedIfNotExisting<T>(this IDependencyInjectionData diService, Func<T?> factoryMethod) where T : class
+        {
             var implFactory = Lib.MakeServiceFactory(factoryMethod);
             InjectScopedIfNotExisting(diService, implFactory);
+        }
+
+        public static Expression<Func<T, bool>> UpdateExpression<T>(this Expression<Func<T, bool>>? originalExpression, Expression<Func<T, bool>> additionalExpression, bool _and)
+        {
+            if (originalExpression == null)
+            {
+                return additionalExpression;
+            }
+            // get the visitor
+            var visitor = new ParameterUpdateVisitor(originalExpression.Parameters.First(), additionalExpression.Parameters.First());
+            // replace the parameter in the original expression
+            originalExpression = visitor.Visit(originalExpression) as Expression<Func<T, bool>>;
+            // now you can and together the two expressions
+            BinaryExpression? binExp;
+            if (_and)
+            {
+                binExp = Expression.And(additionalExpression.Body, originalExpression.Body);
+            }
+            else
+            {
+                binExp = Expression.Or(additionalExpression.Body, originalExpression.Body);
+            }
+            // and return a new lambda, that will do what you want.
+            // NOTE that the binExp has reference only to te newExp.Parameters[0] (there is only 1) parameter, and no other
+            return Expression.Lambda<Func<T, bool>>(binExp, originalExpression.Parameters);
+        }
+
+        public static T[] GetEnumArray<T>(this int[]? ints) where T : Enum
+        {
+            if (ints.IsNullOrEmpty()) return Array.Empty<T>();
+            T[] results = new T[ints.Length];
+            for (int i = 0; i < ints.Length; i++)
+            {
+                results[i] = (T)(object)ints[i];
+            }
+            return results;
         }
     }
 }
---
+++ Internal/ParameterUpdateVisitor.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+
+namespace Cyh.Net.Internal
+{
+    /// <summary>
+    /// updates the parameter in the expression
+    /// </summary>
+    internal class ParameterUpdateVisitor : ExpressionVisitor
+    {
+        private ParameterExpression _oldParameter;
+        private ParameterExpression _newParameter;
+
+        public ParameterUpdateVisitor(ParameterExpression oldParameter, ParameterExpression newParameter)
+        {
+            _oldParameter = oldParameter;
+            _newParameter = newParameter;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            if (object.ReferenceEquals(node, _oldParameter))
+                return _newParameter;
+
+            return base.VisitParameter(node);
+        }
+    }
+}
